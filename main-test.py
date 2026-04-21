dd72f0778340a9193aa754c249d3b2e7a7844491
ShubhamJB <bshubham24@tbc.edu.np>
2026-04-21 21:55:40 +0545

    trigger ci

--- main-test.py
+++ main-test.py
@@ -2,7 +2,7 @@ from main import Add, Subtract
 
 def test_add():
     assert Add(2,3) == 5
-    assert Add(5,5) == 11
+    assert Add(5,6) == 11
 
 def test_subtract():
     assert Subtract(5,2) == 3
@@ -11,3 +11,7 @@ if __name__ == "__main__":
     test_add()
     test_subtract()
     print("All tests passed")
+    
+    #step1 : git add .
+    #step2 : git commit --allow-empty -m "trigger ci"
+    #step3 : git push 
